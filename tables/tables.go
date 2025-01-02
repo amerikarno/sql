b921055d1495d5c95acd395bce01ce83a0923a49
karn-at-549007798570 <karnake.r@finansiada.com>
2025-01-02 10:55:50 +0700

    upadte 20250102

--- tables/tables.go
+++ tables/tables.go
@@ -4,9 +4,9 @@ import "gorm.io/gorm"
 
 type Table[T any] struct{}
 
-func NewTable[T any]() *Table[T] {
-	return &Table[T]{}
-}
+// func NewTable[T any]() *Table[T] {
+// 	return &Table[T]{}
+// }
 
 func (t *Table[T]) GetAll(db *gorm.DB, scopes ...func(*gorm.DB) *gorm.DB) (results []T, err error) {
 	if err = db.Debug().Scopes(scopes...).Find(&results).Error; err != nil {
